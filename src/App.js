bbfe9a350a6859a9e99fb28b840cf5f447a09517
maciekto <ekstar007@gmail.com>
2021-09-25 20:35:50 +0200

    sdfRevert "Finished Portfolio"
    This reverts commit 096ee8e70485664968bed4586d57d113790bc16f.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import './App.scss';
+import Header from './components/2_layouts/Header';
+import AboutMe from './components/2_layouts/AboutMe';
+import MyWorks from './components/2_layouts/MyWorks';
+import Contact from './components/2_layouts/Contact';
+import Nav from './components/2_layouts/Nav';
 
 function App() {
+  const [NavOpen, setNavOpen] = useState(false);
+  const triggerNav = (type) => {
+    type === true ? setNavOpen(true) : setNavOpen(false);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header triggerNav={triggerNav} NavOpen={NavOpen} />
+      <AboutMe />
+      <MyWorks />
+      <Contact />
+      <Nav NavOpen={NavOpen} triggerNav={triggerNav}/>
     </div>
   );
 }
